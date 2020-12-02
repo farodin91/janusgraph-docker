f20ef110d420504be37bbf6aa4695a375c174d24
Jan Jansen <jan.jansen@gdata.de>
2020-12-02 10:02:46 +0100

    bla

--- build-images.sh
+++ build-images.sh
@@ -27,16 +27,16 @@ REVISION=$(git rev-parse --short HEAD)
 CREATED=$(date -u +”%Y-%m-%dT%H:%M:%SZ”)
 
 for v in "${versions[@]}"; do
-  if [ -z "${version}" ] || [ "${version}" == "${v}" ]; then
-    # read full version from Dockerfile
-    full_version=$(grep "ARG JANUS_VERSION" ${v}/Dockerfile | head -n 1 | cut -d"=" -f 2)
-    # build and test image
-    docker build -f "${v}/Dockerfile" -t "janusgraph/janusgraph:${full_version}" ${v} --build-arg REVISION=$REVISION --build-arg CREATED=$CREATED
-    ./test-image.sh "janusgraph/janusgraph:${full_version}"
-    # add relevant tags
-    docker tag "janusgraph/janusgraph:${full_version}" "janusgraph/janusgraph:${v}"
-    if [ "${v}" == "${latest_version}" ]; then
-      docker tag "janusgraph/janusgraph:${v}" "janusgraph/janusgraph:latest"
+    if [ -z "${version}" ] || [ "${version}" == "${v}" ]; then
+        # read full version from Dockerfile
+        full_version=$(grep "ARG JANUS_VERSION" ${v}/Dockerfile | head -n 1 | cut -d"=" -f 2)
+        # build and test image
+        docker build -f "${v}/Dockerfile" -t "janusgraph/janusgraph:${full_version}" ${v} --build-arg REVISION=$REVISION --build-arg CREATED=$CREATED
+        ./test-image.sh "janusgraph/janusgraph:${full_version}"
+        # add relevant tags
+        docker tag "janusgraph/janusgraph:${full_version}" "janusgraph/janusgraph:${v}"
+        if [ "${v}" == "${latest_version}" ]; then
+            docker tag "janusgraph/janusgraph:${v}" "janusgraph/janusgraph:latest"
+        fi
     fi
-  fi
 done
